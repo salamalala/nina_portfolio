bf8d2e0224f28f25b8bc961d74f2df0bfabf82f9
salamalala <hello@ninaregli.com>
2021-03-25 15:12:55 +0100

    feat(client): style client section, make funny shape button

--- assets/js/main.js
+++ assets/js/main.js
@@ -25,6 +25,11 @@ var clientAccordion = function() {
     const number = clientButton.dataset.indexNumber;
     let associatedSection = client.querySelector('.client__information');
     const text = associatedSection.querySelector('.client__textWrapper');
+    // const iframe = client.querySelector('iframe');
+    // var iframeSrc = iframe.src;
+    // console.log(iframe);
+    // const iframeSrc = iframe.src
+    // console.log(iframeSrc);
 
     clientButton.addEventListener('click', (e) => {
 
@@ -37,27 +42,34 @@ var clientAccordion = function() {
           client.classList.remove('active');
 
           clientButton.setAttribute('aria-expanded', false);
-          gsap.to(clientButton, {scale: 0.6, duration: 0.2});
+          gsap.to(clientButton, {scale: 0.6, duration: 0.2, y: 0});
           associatedSection.classList.add("hide");
+          // iframeSrc = iframeSrc.slice(0, -11);
         }
       })
 
       // give the clicked one the class active
       client.classList.toggle('active');
 
-      // if active do this, else do tthis
+      // if active do this, else do this
       if (client.classList.contains('active')) {
-        client.querySelector('iframe').src += "?autoplay=1";
-        gsap.to(clientButton, {scale: 1, duration: 0.2});
+        gsap.to(clientButton, {scale: 1, duration: 0.2, y: 30});
         gsap.fromTo(text, {opacity: 0}, {opacity: 1, duration: 0.5});
 
         clientButton.setAttribute('aria-expanded', true);
 
         associatedSection.classList.remove("hide");
+        // console.log(iframeSrc);
+        // iframeSrc += "&autoplay=1";
+        // console.log(iframeSrc);
+
       } else {
         clientButton.setAttribute('aria-expanded', false);
-        gsap.to(clientButton, {scale: 0.6, duration: 0.2});
+        gsap.to(clientButton, {scale: 0.6, duration: 0.2, y: 0});
         associatedSection.classList.add("hide");
+        // iframeSrc = iframeSrc.slice(0, -11);
+        // // iframe.src.substring(0, iframe.src.length-11);
+        // console.log(iframeSrc);
       }
 
     });
