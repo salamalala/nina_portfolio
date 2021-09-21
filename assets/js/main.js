53e564fda2f009553fd6f78ad8f027dbb18bd526
Nina Regli <hello@ninaregli.com>
2021-09-21 16:56:22 +0200

    feat(work): make it partly responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,22 +14,18 @@ var clientAccordion = function() {
   const clients = document.querySelectorAll('.client');
   let clientButtons = document.querySelectorAll('.client__button');
 
-
   clients.forEach(client => {
 
     let clientButton = client.querySelector('.client__button');
+    let clientButtonText = client.querySelector('.client__buttonText');
 
-    gsap.set(clientButton, {scale: 0.6});
+    gsap.set(clientButtonText, {scale: 0.6});
 
     const expanded = clientButton.getAttribute('aria-expanded');
     const number = clientButton.dataset.indexNumber;
     let associatedSection = client.querySelector('.client__information');
     const text = associatedSection.querySelector('.client__textWrapper');
-    // const iframe = client.querySelector('iframe');
-    // var iframeSrc = iframe.src;
-    // console.log(iframe);
-    // const iframeSrc = iframe.src
-    // console.log(iframeSrc);
+    let plus = client.querySelector('.client__button--plus');
 
     clientButton.addEventListener('click', (e) => {
 
@@ -37,14 +33,16 @@ var clientAccordion = function() {
       clients.forEach(client => {
         if (client !== e.target.parentNode) {
           let clientButton = client.querySelector('.client__button');
+          let clientButtonText = client.querySelector('.client__buttonText');
           let associatedSection = client.querySelector('.client__information');
+          let plus = client.querySelector('.client__button--plus');
 
           client.classList.remove('active');
 
           clientButton.setAttribute('aria-expanded', false);
-          gsap.to(clientButton, {scale: 0.6, duration: 0.2, y: 0});
+          gsap.to(clientButtonText, {scale: 0.6, duration: 0.2, y: 0});
+          gsap.set(plus, {opacity: 1});
           associatedSection.classList.add("hide");
-          // iframeSrc = iframeSrc.slice(0, -11);
         }
       })
 
@@ -53,23 +51,20 @@ var clientAccordion = function() {
 
       // if active do this, else do this
       if (client.classList.contains('active')) {
-        gsap.to(clientButton, {scale: 1, duration: 0.2, y: 30});
+        gsap.to(clientButtonText, {scale: 1, duration: 0.2, y: 30});
         gsap.fromTo(text, {opacity: 0}, {opacity: 1, duration: 0.5});
+        gsap.to(plus, {opacity: 0, duration: 0.2});
 
         clientButton.setAttribute('aria-expanded', true);
 
         associatedSection.classList.remove("hide");
-        // console.log(iframeSrc);
-        // iframeSrc += "&autoplay=1";
-        // console.log(iframeSrc);
+
 
       } else {
+        gsap.set(plus, {opacity: 1, duration: 0.2});
         clientButton.setAttribute('aria-expanded', false);
-        gsap.to(clientButton, {scale: 0.6, duration: 0.2, y: 0});
+        gsap.to(clientButtonText, {scale: 0.6, duration: 0.2, y: 0});
         associatedSection.classList.add("hide");
-        // iframeSrc = iframeSrc.slice(0, -11);
-        // // iframe.src.substring(0, iframe.src.length-11);
-        // console.log(iframeSrc);
       }
 
     });
@@ -79,7 +74,6 @@ var clientAccordion = function() {
 document.addEventListener('DOMContentLoaded', function() {
 
   cursor();
-
   clientAccordion();
 
 
